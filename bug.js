a10b0a22c8d8be733f644b4fd1bce5298a87b0e3
BugHunter-X <askzenai@gmail.com>
2025-01-01 19:08:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when you are trying to access a state variable before it has been initialized. This is a common mistake when working with asynchronous operations or when using functional components with useState hook.
